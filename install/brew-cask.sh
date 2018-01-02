ac93e577c2dbe9a700df52fe67059636d33c8d90
Chris Barrese <chris.barrese@gmail.com>
2018-01-01 18:48:54 -0700

    Create brew-cask.sh

---
+++ install/brew-cask.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Install Caskroom
+brew tap caskroom/cask
+brew install brew-cask
+brew tap caskroom/versions
+
+# Install packages
+apps=(
+    1password
+    google-drive
+    spectacle
+    flux
+    dash
+    evernote
+    iterm2
+    intellij-idea
+    firefox
+    firefoxnightly
+    google-chrome
+    google-chrome-canary
+    hammerspoon
+    macdown
+    opera
+    screenflow
+    spotify
+    skype
+    slack
+    tower
+    transmit
+)
+
+brew cask install "${apps[@]}"
+
+# Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
